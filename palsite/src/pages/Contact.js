22afd604b51eb6f17f141b326b485db5e18612ac
Michael Callahan <michaelfc8841@gmail.com>
2024-05-28 12:52:03 -0400

    Added Icons and sound fx to Contact page

--- palsite/src/pages/Contact.js
+++ palsite/src/pages/Contact.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import './Contact.css'; // Make sure to import the CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faCode } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Contact.css';
+import orbSound from '../assets/orb.mp3'; // Adjust the path as necessary
 
 export default function Contact() {
-    return(
+    const playSound = () => {
+        const audio = new Audio(orbSound);
+        audio.play();
+    };
+
+    return (
         <div>
             <h1>Contact</h1>
             <h5>
-                email: <a href="mailto:michaelfc8841@gmail.com" className="contact-link">michaelfc8841@gmail.com</a>
+                <FontAwesomeIcon icon={faEnvelope} /> email: <a href="mailto:michaelfc8841@gmail.com" className="contact-link" onClick={playSound}>michaelfc8841@gmail.com</a>
             </h5>
             <h5>
-                github: <a href="https://github.com/pal4569" target="_blank" rel="noopener noreferrer" className="contact-link">pal4569</a>
+                <FontAwesomeIcon icon={faGithub} /> github: <a href="https://github.com/pal4569" target="_blank" rel="noopener noreferrer" className="contact-link" onClick={playSound}>pal4569</a>
+            </h5>
+            <h5>
+                <FontAwesomeIcon icon={faLinkedin} /> linkedin: <a href="https://www.linkedin.com/in/michael-f-callahan/" target="_blank" rel="noopener noreferrer" className="contact-link" onClick={playSound}>Michael F Callahan</a>
+            </h5>
+            <h5>
+                <FontAwesomeIcon icon={faCode} /> leetcode: <a href="https://leetcode.com/u/pal4569/" target="_blank" rel="noopener noreferrer" className="contact-link" onClick={playSound}>pal4569</a>
             </h5>
         </div>
-    )
+    );
 }
